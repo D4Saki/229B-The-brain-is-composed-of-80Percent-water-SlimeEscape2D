ac6f02576c36e95e06d744608fb3a6025598d86a
catnip4889 <bking.ywing@gmail.com>
2026-05-04 21:13:20 +0700

    Credits Scroll Succeed

---
+++ Assets/Scripts/Credit.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsScroll : MonoBehaviour
+{
+    [Header("Scroll")]
+    public float speed = 50f;
+
+    [Header("Timer")]
+    public float duration = 10f;
+
+    [Header("Scene")]
+    public string nextScene = "Open";
+
+    RectTransform rect;
+    float timer = 0f;
+
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+
+        // เริ่มจากล่างจอ
+        rect.anchoredPosition = new Vector2(-262, -327);
+    }
+
+    void Update()
+    {
+        // เลื่อนขึ้น
+        rect.anchoredPosition += Vector2.up * speed * Time.deltaTime;
+
+        // จับเวลา
+        timer += Time.deltaTime;
+
+        if (timer >= duration)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
